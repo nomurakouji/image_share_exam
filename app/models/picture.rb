74a233f872ec17460b4f902b6ecb2b188edd0e3c
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-12 19:16:35 +0900

    UserとPictureひも付け,投稿者名表示の機能

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,6 @@
 class Picture < ApplicationRecord
-    mount_uploader :image, ImageUploader
-    validates :image,  presence: true
-    validates :content, presence: true, length: { maximum: 50 }
+  mount_uploader :image, ImageUploader
+  validates :image,  presence: true
+  validates :content, presence: true, length: { maximum: 50 }
+  belongs_to :user, optional:true
 end
