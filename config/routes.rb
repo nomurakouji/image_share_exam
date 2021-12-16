689af8a56de6d61f2998881d2c73f7d09346d034
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-16 11:21:51 +0900

    root修正:letteropener

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     end
   end
   root to: 'pictures#index'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
