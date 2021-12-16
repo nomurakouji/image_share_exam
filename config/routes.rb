57f85e2e9c22b17e534bc5ed2b47241e33312a3c
kouji nomura <nomurakouji0711@yahoo.co.jp>
2021-12-16 11:13:11 +0900

    デザイン再修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  root to: 'pictures#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
